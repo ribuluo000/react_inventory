ff66852aba94a93e627f2205a605135e1ea1ce6e
Nick <ribuluo000@163.com>
2018-06-24 17:43:45 +0800

    供应商、客户、产品、批次 详情页面；

--- inventory/app/containers/App/index.js
+++ inventory/app/containers/App/index.js
@@ -18,15 +18,20 @@ import MyBillPage from "containers/MyPage/Bill/Loadable";
 
 import MyProviderPage from "containers/MyPage/Provider/Loadable";
 import ProviderAddPage from "containers/MyPage/Provider/ProviderAdd/Loadable";
+import ProviderDetailPage from "containers/MyPage/Provider/ProviderDetail/Loadable";
 
 import MyCustomerPage from "containers/MyPage/Customer/Loadable";
 import CustomerAddPage from "containers/MyPage/Customer/CustomerAdd/Loadable";
+import CustomerDetailPage from "containers/MyPage/Customer/CustomerDetail/Loadable";
 
 
 import MyProductBatchPage from "containers/MyPage/Product/Batch/Loadable";
 import ProductBatchAddPage from "containers/MyPage/Product/Batch/BatchAdd/Loadable";
+import ProductBatchDetailPage from "containers/MyPage/Product/Batch/BatchDetail/Loadable";
+
 import MyProductPage from "containers/MyPage/Product/Loadable";
 import ProductAddPage from "containers/MyPage/Product/ProductAdd/Loadable";
+import ProductDetailPage from "containers/MyPage/Product/ProductDetail/Loadable";
 import HomePage from "containers/HomePage/Loadable";
 import FeaturePage from "containers/FeaturePage/Loadable";
 import NotFoundPage from "containers/NotFoundPage/Loadable";
@@ -60,18 +65,24 @@ export default function App() {
         <Route path="/features" component={FeaturePage}/>
         <Route path="/base_info" component={FeaturePage}/>
         <Route path="/bill" component={MyBillPage}/>
+        <Route path="/bill/add" component={MyBillPage}/>
+        <Route path="/bill/detail" component={MyBillPage}/>
 
         <Route path="/provider" component={MyProviderPage}/>
         <Route path="/provider/add" component={ProviderAddPage}/>
+        <Route path="/provider/detail" component={ProviderDetailPage}/>
 
         <Route path="/customer" component={MyCustomerPage}/>
         <Route path="/customer/add" component={CustomerAddPage}/>
+        <Route path="/customer/detail" component={CustomerDetailPage}/>
 
         <Route path="/product" component={MyProductPage}/>
         <Route path="/product/add" component={ProductAddPage}/>
+        <Route path="/product/detail" component={ProductDetailPage}/>
 
         <Route path="/product/batch" component={MyProductBatchPage}/>
         <Route path="/product/batch/add" component={ProductBatchAddPage}/>
+        <Route path="/product/batch/detail" component={ProductBatchDetailPage}/>
 
 
         <Route path="" component={NotFoundPage}/>
---
+++ inventory/app/containers/MyPage/Customer/CustomerDetail/ViewIndex.js
@@ -0,0 +1,211 @@
+/*
+ * My Bill Page
+ *
+ */
+
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { Icon, List, NavBar, SearchBar, Text, View, InputItem, TextareaItem } from "antd-mobile";
+import BaseComponent from "containers/Base/BaseComponent";
+import { FormattedMessage } from "react-intl";
+import messages from "containers/App/messages";
+import MyButton from "components/MyButton/";
+const Item = List.Item;
+const Brief = Item.Brief;
+/* eslint-disable react/prefer-stateless-function */
+export default class ViewIndex extends BaseComponent {
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+    const {
+      user_name,
+      data,
+
+      onChange_name,
+      onChange_telephone,
+      onChange_remark,
+      name,
+      telephone,
+      remark,
+
+      onPress__button__back,
+
+      onPress__button__add,
+
+      onPress__button__done,
+
+      onPress__button__edit,
+
+      onPress__list_item,
+
+      onPress__button__search,
+
+    } = this.props;
+
+    let dataList = [];
+    if (data && data.dataList) {
+      dataList = data.dataList;
+    } else {
+      dataList = [
+        {
+          key : 'key',
+          title : 'title',
+          subtitle : 'subtitle',
+          extra : 'extra',
+        },
+        {
+          key : 'key2',
+          title : 'title2',
+          subtitle : 'subtitle',
+          extra : 'extra',
+        },
+      ];
+    }
+
+    return (
+      <View>
+        <Helmet>
+          {/*<title>{intl.formatMessage(messages.button__base_info)}</title>*/}
+          <title>账单</title>
+        </Helmet>
+        <NavBar
+          mode="dark"
+          icon={<Icon type="left"/>}
+          onLeftClick={onPress__button__back}
+          rightContent={[
+
+            <FormattedMessage
+              key={messages.edit.id}
+              {...messages.edit}>
+              {
+                msg => (
+                  <MyButton
+                    // type="primary"
+                    inline={false}
+                    size="small"
+                    onClick={onPress__button__edit}
+                  >
+                    {msg}
+                  </MyButton>
+                )
+              }
+
+            </FormattedMessage>,
+
+            <FormattedMessage
+              key={messages.done.id}
+              {...messages.done}>
+              {
+                msg => (
+                  <MyButton
+                    // type="primary"
+                    inline={false}
+                    size="small"
+                    onClick={onPress__button__done}
+                  >
+                    {msg}
+                  </MyButton>
+                )
+              }
+
+            </FormattedMessage>,
+
+          ]}
+        >
+          <FormattedMessage {...messages.customer_detail}>
+            {
+              msg => (
+                <Text>
+                  {msg}
+                </Text>
+              )
+            }
+
+          </FormattedMessage>
+        </NavBar>
+
+        <List>
+
+
+          <FormattedMessage {...messages.name}>
+            {
+              msg => (
+                <InputItem
+                  id="name"
+                  type="text"
+                  value={name}
+                  onChange={onChange_name}
+                  placeholder={msg}
+                />
+              )
+            }
+
+          </FormattedMessage>
+
+          <FormattedMessage {...messages.telephone}>
+            {
+              msg => (
+                <InputItem
+                  id="telephone"
+                  type="text"
+                  value={telephone}
+                  onChange={onChange_telephone}
+                  placeholder={msg}
+                />
+              )
+            }
+
+          </FormattedMessage>
+
+
+          <FormattedMessage {...messages.remark}>
+            {
+              msg => (
+                <TextareaItem
+                  id="remark"
+                  value={remark}
+                  onChange={onChange_remark}
+                  placeholder={msg}
+                  rows={5}
+                  count={100}
+                />
+              )
+            }
+
+          </FormattedMessage>
+
+        </List>
+      </View>
+    );
+  }
+}
+
+ViewIndex.propTypes = {
+  loading : PropTypes.bool,
+  error : PropTypes.oneOfType([ PropTypes.object, PropTypes.bool ]),
+  repos : PropTypes.oneOfType([ PropTypes.array, PropTypes.bool ]),
+  onPress_login : PropTypes.func,
+  user_name : PropTypes.string,
+  onChange_name : PropTypes.func,
+  onChange_telephone : PropTypes.func,
+  onChange_remark : PropTypes.func,
+
+  name: PropTypes.string,
+  telephone: PropTypes.string,
+  remark: PropTypes.string,
+
+
+  data : PropTypes.object,
+
+};
+
+
