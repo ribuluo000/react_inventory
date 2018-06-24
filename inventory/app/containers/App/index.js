8a7b6113399bfb215f012278e59e6ebdd7d617f9
Nick <ribuluo000@163.com>
2018-06-24 17:19:29 +0800

    添加供应商、客户、产品、批次页面；

--- inventory/app/containers/App/index.js
+++ inventory/app/containers/App/index.js
@@ -15,10 +15,18 @@ import PrivateRoute from "containers/Base/PrivateRoute";
 import LoginPage from "containers/LoginPage/Loadable";
 import MyPage from "containers/MyPage/Loadable";
 import MyBillPage from "containers/MyPage/Bill/Loadable";
+
 import MyProviderPage from "containers/MyPage/Provider/Loadable";
+import ProviderAddPage from "containers/MyPage/Provider/ProviderAdd/Loadable";
+
 import MyCustomerPage from "containers/MyPage/Customer/Loadable";
+import CustomerAddPage from "containers/MyPage/Customer/CustomerAdd/Loadable";
+
+
 import MyProductBatchPage from "containers/MyPage/Product/Batch/Loadable";
+import ProductBatchAddPage from "containers/MyPage/Product/Batch/BatchAdd/Loadable";
 import MyProductPage from "containers/MyPage/Product/Loadable";
+import ProductAddPage from "containers/MyPage/Product/ProductAdd/Loadable";
 import HomePage from "containers/HomePage/Loadable";
 import FeaturePage from "containers/FeaturePage/Loadable";
 import NotFoundPage from "containers/NotFoundPage/Loadable";
@@ -45,17 +53,27 @@ export default function App() {
       </Helmet>
       <Header />
       <Switch>
-        <Route exact path="/" component={LoginPage}/>
+        <Route exact path="/" component={ProductBatchAddPage}/>
         <Route path="/login" component={LoginPage}/>
         <Route path="/my" component={MyPage}/>
         <PrivateRoute path="/home" component={HomePage}/>
         <Route path="/features" component={FeaturePage}/>
         <Route path="/base_info" component={FeaturePage}/>
         <Route path="/bill" component={MyBillPage}/>
+
         <Route path="/provider" component={MyProviderPage}/>
+        <Route path="/provider/add" component={ProviderAddPage}/>
+
         <Route path="/customer" component={MyCustomerPage}/>
+        <Route path="/customer/add" component={CustomerAddPage}/>
+
         <Route path="/product" component={MyProductPage}/>
+        <Route path="/product/add" component={ProductAddPage}/>
+
         <Route path="/product/batch" component={MyProductBatchPage}/>
+        <Route path="/product/batch/add" component={ProductBatchAddPage}/>
+
+
         <Route path="" component={NotFoundPage}/>
       </Switch>
       <Footer />
---
+++ inventory/app/containers/MyPage/Provider/ProviderAdd/reducer.js
@@ -0,0 +1,45 @@
+/*
+ * loginReducer
+ *
+ * The reducer takes care of our data. Using actions, we can change our
+ * application state.
+ * To add a new action, add it to the switch statement in the reducer function
+ *
+ * Example:
+ * case YOUR_ACTION_CONSTANT:
+ *   return state.set('yourStateVariable', true);
+ */
+import { fromJS } from "immutable";
+
+import {
+  /********************************************************* network start ******************************************************************/
+
+  /********************************************************** network end ******************************************************************/
+
+
+} from "./constants";
+
+// The initial state of the App
+export const initialState = fromJS({
+
+
+  /****************************** network start **************************************/
+
+
+  /****************************** network end **************************************/
+
+});
+
+function myReducer(state = initialState, action) {
+  switch (action.type) {
+
+    /****************************** network start **************************************/
+
+    /****************************** network end **************************************/
+
+    default:
+      return state;
+  }
+}
+
+export default myReducer;
