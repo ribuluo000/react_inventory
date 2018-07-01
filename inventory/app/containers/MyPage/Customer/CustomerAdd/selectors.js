8491fc9eb28dd6c5bb41550b4de6e46a721eaf5c
Nick <ribuluo000@163.com>
2018-07-01 19:26:03 +0800

    产品添加接口；

--- inventory/app/containers/MyPage/Customer/CustomerAdd/selectors.js
+++ inventory/app/containers/MyPage/Customer/CustomerAdd/selectors.js
@@ -5,7 +5,7 @@
 import { createSelector } from "reselect";
 import { initialState } from "./reducer";
 
-const select = state => state.get(PATH.PATH__provider__add, initialState);
+const select = state => state.get(PATH.PATH__customer__add, initialState);
 
 const makeSelect__name = () =>
   createSelector(select, state => state.get('name'));
